92f43df7c0fc45032bf41a22882c3ee8d43971d5
Chibuike Ewenike <sopewenike@yahoo.com>
2023-09-15 14:24:47 +0100

    team layout improved

--- src/components/Team/TeamBuilder.js
+++ src/components/Team/TeamBuilder.js
@@ -6,13 +6,14 @@ function TeamBox({ teams }) {
     <>
       <div className="md:grid md:grid-cols-2 lg:grid-cols-4 gap-5 mx-auto">
         {(teams || []).map((team, key) => (
-          <div className="relative hover:top-[-8px]" key={key}>
-            <div className="mx-auto relative md:w-[21rem] md:h-[21rem] lg:h-[13.5rem] lg:w-[13.5rem] rounded">
+          <div className="relative w-full hover:top-[-8px]" key={key}>
+            <div className="grid place-items-center relative md:w-[21rem] md:h-[21rem] lg:h-[13.5rem] lg:w-[13.5rem] rounded">
               <Image
                 alt="team member image"
                 src={team.image}
                 width="216"
                 height="216"
+                className=""
                 // layout="responsive"
               />
             </div>
@@ -20,7 +21,7 @@ function TeamBox({ teams }) {
             <h4 className="text-base pt-5 text-center mb-[7px]">
               <Link href={team.link}>{team.title}</Link>
             </h4>
-            <p className="text-[#a8aeb4] text-center text-[13px] ">
+            <p className="text-[#630b0b] text-center text-[13px] ">
               {team.desc}
             </p>
           </div>
